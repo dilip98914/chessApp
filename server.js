624ba1a5213f3e0e6e963070e710a3983b5cc3ea
dilip98914 <dilip98914@gmail.com>
2020-01-04 22:19:17 +0530

    getting it worked

--- server.js
+++ server.js
@@ -1,32 +1,46 @@
 const express = require('express')
+const  Chess = require('chess.js').Chess;
 const app = express()
+const server=require('http').createServer(app)
 const port = 3000
-
 const mongoose =require('mongoose')
+const socketIO=require('socket.io')
+const io=socketIO(server)
+
+app.set('view engine','ejs')
+app.use(express.static('public'))
+
 mongoose.connect('mongodb://dilip:dilip123@dbh43.mlab.com:27437/chessdb', {useNewUrlParser: true})
 .catch(err=>{
     console.log(err)
 })
 
-app.set('view engine','ejs')
 
-app.use(express.static('public'))
-
-var Chess = require('chess.js').Chess;
+//chess logic
 var chess = new Chess();
 
-while (!chess.game_over()) {
-  var moves = chess.moves();
-  var move = moves[Math.floor(Math.random() * moves.length)];
-  chess.move(move);
-}
-console.log(chess.pgn());
 
-app.get('/', (req, res) =>{
-    res.render('index',{data:{
-        name:'dilip',
-        age:34
+//socket logic
+io.on('connection',(socket)=>{
+    console.log('new user connected!');
+    socket.emit('sendBoard',{
+        'board':chess.fen()
+    });
+})
+
+
+app.get('/chess', (req, res) =>{
+    // while (!chess.game_over()) {
+    var moves = chess.moves();
+    var move = moves[Math.floor(Math.random() * moves.length)];
+    chess.move(move);
+    var toPass=chess.fen();
+    //   }
+      // console.log(chess.pgn());
+      
+    res.render('index',{chess:{
+        board:toPass,
     }})
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+server.listen(port, () => console.log(`Example app listening on port ${port}!`))
