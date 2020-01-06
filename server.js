047b1319a31721db0b06881c627ef54199e17ed1
dilip98914 <dilip98914@gmail.com>
2020-01-06 18:46:44 +0530

    confused y piece is not moving

--- server.js
+++ server.js
@@ -1,43 +1,44 @@
 const express = require('express')
 // const  Chess = require('chess.js').Chess;
 const app = express()
-const server=require('http').createServer(app)
+const server = require('http').createServer(app)
 const port = 3000
-const mongoose =require('mongoose')
-const socketIO=require('socket.io')
-const io=socketIO(server)
+const mongoose = require('mongoose')
+const socketIO = require('socket.io')
+const io = socketIO(server)
 
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-mongoose.connect('mongodb://dilip:dilip123@dbh43.mlab.com:27437/chessdb', {useNewUrlParser: true})
-.catch(err=>{
-    console.log(err)
-})
+mongoose.connect('mongodb://dilip:dilip123@dbh43.mlab.com:27437/chessdb', { useNewUrlParser: true })
+    .catch(err => {
+        console.log(err)
+    })
 
+users = [];
+connections = [];
 
-//chess logic
-// var chess = new Chess();
+io.on('connection', (socket) => {
+    connections.push(socket);
+    console.log('new user connected! %s %s connections',socket.id, connections.length);
 
+    socket.on('send game', (state) => {
+        console.log(state);
+        socket.emit('new game', {state:state});
+    });
+    socket.on('update status', (state) => {
+        console.log(state);
+        socket.emit('new update', {state:state});
+    });
 
-//socket logic
-io.on('connection',(socket)=>{
-    console.log('new user connected!');
-    socket.on('gameStatus',(obj)=>{
-      console.log(obj)
+    socket.on('disconnect', () => {
+        connections.splice(connections.indexOf(socket));
+        console.log(' user dissconnected ! %s connections', connections.length);
     });
 })
 
 
-app.get('/chess', (req, res) =>{
-    // while (!chess.game_over()) {
-    // var moves = chess.moves();
-    // var move = moves[Math.floor(Math.random() * moves.length)];
-    // chess.move(move);
-    // var toPass=chess.fen();
-    // //   }
-    //   // console.log(chess.pgn());
-      
+app.get('/chess', (req, res) => {
     res.render('index')
 })
 
